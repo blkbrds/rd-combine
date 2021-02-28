b72dd10a3ea1ca1d03c063d5c56baaddf6d7a829
Huy Vo D.[2] <huy.vo2@monstar-lab.com>
2021-02-28 18:41:23 +0700

    Completed question 1

---
+++ Examinations and projects/Test 1/HuyVoD/CombineTest1/NotificationNameExt.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNameExt.swift
+//  CombineTest1
+//
+//  Created by MBA0288F on 2/28/21.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let moveEdit = Notification.Name("moveEdit")
+    static let editSuccess = Notification.Name("editSuccess")
+}
