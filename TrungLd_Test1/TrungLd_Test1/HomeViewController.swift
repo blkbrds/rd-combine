21bd54d9654fefb5b455d95ce9356e3ef7ffd42d
Trung Le D <trung.le@monstar-lab.com>
2021-02-28 20:55:39 +0700

    Init repo

---
+++ TrungLd_Test1/TrungLd_Test1/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  TrungLd_Test1
+//
+//  Created by Trung Le D. on 28/02/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
