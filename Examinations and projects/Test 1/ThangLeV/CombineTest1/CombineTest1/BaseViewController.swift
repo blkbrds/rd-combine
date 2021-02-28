d7b4e7289038b7ea003df8d8cec7b8666c65660a
Thang Le V <thang.le@monstar-lab.com>
2021-02-28 10:10:11 +0700

    Complete test 1

---
+++ Examinations and projects/Test 1/ThangLeV/CombineTest1/CombineTest1/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  CombineTest1
+//
+//  Created by MBA0183 on 2/26/21.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default) //UIImage.init(named: "transparent.png")
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.view.backgroundColor = .clear
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
