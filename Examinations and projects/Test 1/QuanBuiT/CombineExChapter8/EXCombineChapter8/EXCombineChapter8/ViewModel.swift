1a0f27eda476471de7d0cb7050c613dc5b99074b
Quan Bui T <quan.bui@monstar-lab.com>
2021-04-03 12:24:13 +0700

    Quan Bui T. group2 commit exercise chapter8

---
+++ Examinations and projects/Test 1/QuanBuiT/CombineExChapter8/EXCombineChapter8/EXCombineChapter8/ViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  ViewModel.swift
+//  EXCombineChapter8
+//
+//  Created by MBA0253P on 4/3/21.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+final class ViewModel {
+    // MARK: - enum
+    enum State {
+        case initial
+        case fetched
+        case failed(Error)
+    }
+
+    enum Action {
+        case fetchData
+        case clear
+    }
+    
+    let action = PassthroughSubject<Action, Never>()
+    let state = CurrentValueSubject<State, Never>(.initial)
+
+    var subscriptions = Set<AnyCancellable>()
+    let images = CurrentValueSubject<[UIImage], Never>([])
+
+    // MARK: - Init
+    init() {
+        state
+            .sink { [weak self] in
+                self?.processState($0)
+            }
+            .store(in: &subscriptions)
+        action
+            .sink { [weak self] action in
+                self?.processAction(action)
+            }
+            .store(in: &subscriptions)
+    }
+
+    // MARK: - Function
+    private func getImage() -> [UIImage] {
+        var images: [UIImage] = []
+        for index in 1...6 {
+            if let imageItem = UIImage(named: "img-" + String(index)) {
+                images.append(imageItem)
+            }
+        }
+        return images
+    }
+
+    private func processAction(_ action: Action) {
+        switch action {
+        case .fetchData:
+            images.send(getImage())
+        case .clear:
+            images.send([])
+        }
+    }
+
+    private func processState(_ state: State) {
+        switch state {
+        case .initial:
+            break
+        case .fetched:
+            break
+        case .failed:
+           break
+        }
+    }
+}
