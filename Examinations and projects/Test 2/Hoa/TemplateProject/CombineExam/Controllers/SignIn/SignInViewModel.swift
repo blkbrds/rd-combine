0a4ee907a3b3c544215e8629306eb7bc308b1ef0
Hoa Nguyen X <hoa.nguyen2@monstar-lab.com>
2021-04-13 16:05:14 +0700

    Test2

---
+++ Examinations and projects/Test 2/Hoa/TemplateProject/CombineExam/Controllers/SignIn/SignInViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  SignInViewModel.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import Foundation
+import Combine
+
+final class SignInViewModel {
+
+    @Published var isValid: Bool = false
+    var subscriptions = Set<AnyCancellable>()
+    let user = User(name: "name", address: "Da Nang")
+    let emailPublisher = CurrentValueSubject<String, Never>("")
+    let pwdPublisher = CurrentValueSubject<String, Never>("")
+
+
+    init() {
+        send()
+    }
+
+    func isValidEmail() -> Bool {
+        if emailPublisher.value.count <= 20,
+           emailPublisher.value.count >= 2,
+           !emailPublisher.value.containsEmoji,
+           emailPublisher.value == user.name,
+           pwdPublisher.value.count >= 8,
+           pwdPublisher.value.count <= 20,
+           pwdPublisher.value == user.password
+        {
+            return true
+        }
+        return false
+    }
+
+    func send() {
+        emailPublisher.combineLatest(pwdPublisher).sink { (email, pass) in
+            if self.isValidEmail() {
+                self.isValid = true
+            } else {
+                self.isValid = false
+            }
+        }.store(in: &subscriptions)
+    }
+}
+
