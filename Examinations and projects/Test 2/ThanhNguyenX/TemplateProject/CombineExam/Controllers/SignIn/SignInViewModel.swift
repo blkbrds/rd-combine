ceaaba8a0033b03825976dc312f0fa3faf83d985
Thanh Nguyen X. [4] <thanh.nguyen4@monstar-lab.com>
2021-04-13 16:53:42 +0700

    Finish test 2 part 1

---
+++ Examinations and projects/Test 2/ThanhNguyenX/TemplateProject/CombineExam/Controllers/SignIn/SignInViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewModel.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import Foundation
+
+final class SignInViewModel {
+    var username: String = ""
+    var password: String = ""
+}
---
+++ Examinations and projects/Test 2/ThanhNguyenX/TemplateProject/CombineExam/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        let vc = SignInViewController()
+        vc.viewModel = SignInViewModel()
+        window.rootViewController = UINavigationController(rootViewController: vc)
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
