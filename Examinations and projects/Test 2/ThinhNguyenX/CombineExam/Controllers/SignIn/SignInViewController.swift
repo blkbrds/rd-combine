8d3e5839a22b110eeb80f73775b31a207703a2ac
Thinh Nguyen X <thinh.nguyen2@monstar-lab.com>
2021-04-13 16:58:34 +0700

    Create file

---
+++ Examinations and projects/Test 2/ThinhNguyenX/CombineExam/Controllers/SignIn/SignInViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SignInViewController.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import UIKit
+import Combine
+import SwiftUI
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet private weak var userNameTextField: UITextField!
+    @IBOutlet private weak var pwTextField: UITextField!
+    
+    var viewModel: SignInViewModel?
+    private var subscriptions = Set<AnyCancellable>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UINavigationBar.appearance().prefersLargeTitles = true
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().tintColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().backgroundColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        UINavigationBar.appearance().shadowImage = UIImage()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+
+    private func setupBindings() {
+        // usernameTextField
+        userNameTextField.publisher
+            .assign(to: \.user, on: viewModel)
+            .store(in: &subscriptions)
+    }
+
+    @IBAction private func signInButtonTouchUpInside(_ sender: UIButton) {
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.indicatorView.stopAnimating()
+            self.handleSignIn()
+        }
+    }
+    
+    private func handleSignIn() {
+        let vc = HomeViewController()
+        vc.viewModel = HomeViewModel()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension SignInViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+
+        return true
+    }
+}
---
+++ Examinations and projects/Test 2/ThinhNguyenX/CombineExam/Controllers/SignIn/SignInViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  SignInViewModel.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import Foundation
+import Combine
+
+final class SignInViewModel: ObservableObject {
+    @Published var userName: String = ""
+    @Published var passWord: String = ""
+    @Published var isLoading = false
+
+    let validationResult = PassthroughSubject<Void, Error>()
+
+    private(set) lazy var isInputValid = Publishers.CombineLatest($userName, $passWord)
+        .map { ($0.count > 2 && $0.count < 20) && ($1.count > 8 && $1.count < 20) }
+        .eraseToAnyPublisher()
+
+    private let loginValidator: LoginValidatorProtocol
+
+    init(loginValidator: LoginValidatorProtocol = LoginValidator()) {
+        self.loginValidator = loginValidator
+    }
+
+    func validateLogin() {
+        isLoading = true
+        loginValidator.validateLogins(userName: userName, password: passWord) { [weak self] result in
+            guard let this = self else { return }
+            this.isLoading = false
+            switch result {
+            case .success:
+                this.validationResult.send(())
+            case let .failure(error):
+                this.validationResult.send(completion: .failure(error))
+            }
+        }
+    }
+}
+
+// MARK: - LoginValidatorProtocol
+protocol LoginValidatorProtocol {
+    func validateLogins(userName: String, password: String, completion: @escaping (Result<(), Error>) -> Void)
+}
+
+final class LoginValidator: LoginValidatorProtocol {
+    func validateLogins(userName: String, password: String, completion: @escaping (Result<(), Error>) -> Void) {
+        let time: DispatchTime = .now() + .milliseconds(Int.random(in: 200 ... 1_000))
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            // hardcoded success
+            completion(.success(()))
+        }
+    }
+}
