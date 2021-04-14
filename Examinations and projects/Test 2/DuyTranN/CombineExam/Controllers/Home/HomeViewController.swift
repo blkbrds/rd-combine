42dbdc93666ca674ff13349cc1408daf124d9fa1
Duy Tran N <duy.tran@monstar-lab.com>
2021-04-14 16:38:37 +0700

    Implement search for home screen.

--- Examinations and projects/Test 2/DuyTranN/CombineExam/Controllers/Home/HomeViewController.swift
+++ Examinations and projects/Test 2/DuyTranN/CombineExam/Controllers/Home/HomeViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
-    
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var searchTextField: UITextField!
     @IBOutlet private weak var tableView: UITableView!
+
+    // MARK: - Properties
     let identifier = String(describing: "HomeViewCell")
-    
-    var viewModel: HomeViewModel?
-    
+    var viewModel = HomeViewModel()
+    var subscriptions = Set<AnyCancellable>()
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
@@ -21,30 +27,43 @@ class HomeViewController: UIViewController {
         let nib = UINib(nibName: identifier, bundle: Bundle.main)
         tableView.register(nib, forCellReuseIdentifier: identifier)
         tableView.dataSource = self
-        tableView.delegate = self
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+        searchTextField.addTarget(self,
+                                  action: #selector(textFieldEditingChanged(textField:)),
+                                  for: .editingChanged)
+
+        viewModel.searchInputSubject
+            .sink(receiveValue: { [weak self] _ in
+                guard let this = self else { return }
+                this.tableView.reloadData()
+            })
+            .store(in: &subscriptions)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
+
+    // MARK: - IBActions
+    @objc private func textFieldEditingChanged(textField: UITextField) {
+        viewModel.searchInputSubject.send(textField.value)
+    }
 }
 
+// MARK: - Extension UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) else {
             fatalError()
         }
+        let cellVM = viewModel.viewModelForHomeViewCell(at: indexPath)
+        (cell as? HomeViewCell)?.updateView(with: cellVM)
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-}
-
-extension HomeViewController: UITableViewDelegate {
-    
 }
--- Examinations and projects/Test 2/DuyTranN/CombineExam/Controllers/Home/HomeViewModel.swift
+++ Examinations and projects/Test 2/DuyTranN/CombineExam/Controllers/Home/HomeViewModel.swift
@@ -6,7 +6,37 @@
 //
 
 import Foundation
+import Combine
 
 final class HomeViewModel {
-    
+
+    // MARK: - Properties
+    let sourcesUsers = LocalDatabase.users
+    var displayUsers: [User] = []
+    var searchInputSubject = CurrentValueSubject<String, Never>("")
+    var subscriptions = Set<AnyCancellable>()
+
+    // MARK: - Life cycle
+    init() {
+        searchInputSubject
+            .sink(receiveValue: { [weak self] _ in
+                guard let this = self else { return }
+                guard !this.searchInputSubject.value.isEmpty else {
+                    this.displayUsers = this.sourcesUsers
+                    return
+                }
+                this.displayUsers = this.sourcesUsers.filter({ $0.name.contains(this.searchInputSubject.value) })
+            })
+            .store(in: &subscriptions)
+    }
+
+    // MARK: - Public functions
+    func numberOfRows() -> Int {
+        return displayUsers.count
+    }
+
+    func viewModelForHomeViewCell(at indexPath: IndexPath) -> HomeCellViewModel {
+        let user = displayUsers[indexPath.row]
+        return HomeCellViewModel(user: user)
+    }
 }
