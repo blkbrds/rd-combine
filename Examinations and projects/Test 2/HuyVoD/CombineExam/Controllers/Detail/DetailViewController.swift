ca9c3c0bdc84722857d427ff303750656b66e6f2
Huy Vo D.[2] VN.Danang <huy.vo2@monstar-lab.com>
2021-04-14 14:02:01 +0700

    Part 1, handle validation and login

---
+++ Examinations and projects/Test 2/HuyVoD/CombineExam/Controllers/Detail/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Examinations and projects/Test 2/HuyVoD/CombineExam/Utils/SignInError.swift
@@ -0,0 +1,29 @@
+//
+//  LoginError.swift
+//  CombineExam
+//
+//  Created by Van Le H. on 4/11/21.
+//  Copyright © 2021 ___ORGANIZATIONNAME___ All rights reserved.
+//
+
+import Foundation
+
+enum SignInError: Error {
+    case invalidUsernameLength
+    case invalidUsername
+    case invalidPasswordLength
+    case unknown
+
+    var message: String {
+        switch self {
+        case .invalidUsernameLength:
+            return "Username chỉ được chứa từ 6 đến 20 ký tự"
+        case .invalidUsername:
+            return "Username không được phép chứa emoji"
+        case .invalidPasswordLength:
+            return "Password chỉ được chứa từ 8 đến 20 ký tự"
+        case .unknown:
+            return "Lỗi không xác định"
+        }
+    }
+}
