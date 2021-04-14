c8bcf9a8cede122e097f5760c0c9d2416ad7e13b
Quan Bui T <quan.bui@monstar-lab.com>
2021-04-14 16:46:05 +0700

    commit Test 3

--- Examinations and projects/Test 2/QuanBuiT/TemplateProject/CombineExam/Controllers/Home/HomeViewController.swift
+++ Examinations and projects/Test 2/QuanBuiT/TemplateProject/CombineExam/Controllers/Home/HomeViewController.swift
@@ -6,17 +6,32 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
     let identifier = String(describing: "HomeViewCell")
     
     var viewModel: HomeViewModel?
+    var subscriptions: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
+        
+        if let viewModel = viewModel {
+            searchTextField.publisher
+                .compactMap { $0 }
+                .assign(to: \.searchText, on: viewModel)
+                .store(in: &subscriptions)
+            viewModel.users
+                .sink { [weak self] _ in
+                    self?.tableView.reloadData()
+                }
+                .store(in: &subscriptions)
+        }
         
         let nib = UINib(nibName: identifier, bundle: Bundle.main)
         tableView.register(nib, forCellReuseIdentifier: identifier)
@@ -24,6 +39,7 @@ class HomeViewController: UIViewController {
         tableView.delegate = self
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+        tableView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,14 +50,16 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? HomeViewCell, let user = viewModel?.users.value[indexPath.row] else {
             fatalError()
         }
+        cell.nameLabel.text = user.name
+        cell.addressLabel.text = user.address
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel?.users.value.count ?? 0
     }
 }
 
--- Examinations and projects/Test 2/QuanBuiT/TemplateProject/CombineExam/Controllers/Home/HomeViewModel.swift
+++ Examinations and projects/Test 2/QuanBuiT/TemplateProject/CombineExam/Controllers/Home/HomeViewModel.swift
@@ -6,7 +6,23 @@
 //
 
 import Foundation
+import Combine
 
 final class HomeViewModel {
+    var users = CurrentValueSubject<[User], Never>(LocalDatabase.users)
+    @Published var searchText: String?
+    var subscriptions: Set<AnyCancellable> = []
     
+    init() {
+        $searchText
+            .dropFirst()
+            .sink { value in
+                let usersSearch = LocalDatabase.users.filter {
+                    $0.name.lowercased().contains(value?.lowercased() ?? "")
+                  }
+                self.users.send(usersSearch)
+            }
+            .store(in: &subscriptions)
+    }
 }
+
