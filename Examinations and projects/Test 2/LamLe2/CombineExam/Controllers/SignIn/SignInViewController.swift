04b61269baebd9ae4d2ff9e963e51b4539f1801d
Lam Le T <lam.le2@monstar-lab.com>
2021-04-14 16:50:27 +0700

    Không làm được push lên cho có thôi. Ok Vân Lê

---
+++ Examinations and projects/Test 2/LamLe2/CombineExam/Controllers/SignIn/SignInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignInViewController.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    var viewModel: SignInViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UINavigationBar.appearance().prefersLargeTitles = true
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().tintColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().backgroundColor = #colorLiteral(red: 0.3564865291, green: 0.7821497917, blue: 0.9799445271, alpha: 1)
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        UINavigationBar.appearance().shadowImage = UIImage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+
+    @IBAction private func signInButtonTouchUpInside(_ sender: UIButton) {
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.indicatorView.stopAnimating()
+            self.handleSignIn()
+        }
+    }
+    
+    private func handleSignIn() {
+        let vc = HomeViewController()
+        vc.viewModel = HomeViewModel()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
---
+++ Examinations and projects/Test 2/LamLe2/CombineExam/Controllers/SignIn/SignInViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewModel.swift
+//  CombineExam
+//
+//  Created by MBP0051 on 4/11/21.
+//
+
+import Foundation
+
+final class SignInViewModel {
+    
+}
