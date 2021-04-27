d28ce582d7ab8802029f99569f7498300cb8e944
An Nguyen T <an.nguyen2@monstar-lab.com>
2021-04-27 10:06:36 +0700

    Remove max publishers

--- Networking/NetworkingCombine/NetworkingCombine/Module/DrinksView/DrinksViewModel.swift
+++ Networking/NetworkingCombine/NetworkingCombine/Module/DrinksView/DrinksViewModel.swift
@@ -26,20 +26,18 @@ final class DrinksViewModel: ObservableObject {
             .map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })
             .removeDuplicates()
             .print("DrinksViewModel")
-            .flatMap(maxPublishers: .max(1), { text -> AnyPublisher<CocktailNetworkManager.DrinkResponseData, Never> in
+            .flatMap({ text -> AnyPublisher<CocktailNetworkManager.DrinkResponseData, Never> in
                 let emptyDataPublisher = Just(CocktailNetworkManager.DrinkResponseData.init()).eraseToAnyPublisher()
                 if text.isEmpty {
                     return emptyDataPublisher
                 }
                 self.isLoading = true
-                let publisher = self.cocktailNetworkManager.getCocktails(name: text)
+                return self.cocktailNetworkManager.getCocktails(name: text)
                     .catch({ (error) -> AnyPublisher<CocktailNetworkManager.DrinkResponseData, Never> in
                         self.error = APIError.unknow(error.localizedDescription)
                         return emptyDataPublisher
                     })
                     .eraseToAnyPublisher()
-                
-                return publisher
             })
             .sink(receiveValue: { value in
                 self.isLoading = false
