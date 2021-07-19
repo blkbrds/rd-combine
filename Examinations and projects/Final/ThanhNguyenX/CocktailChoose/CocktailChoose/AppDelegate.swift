686ab0678d3102f07a1c8989d634fd0268abf090
Thanh Nguyen X <thanh.nguyen4@monstar-lab.com>
2021-07-19 17:09:33 +0700

    Init project

---
+++ Examinations and projects/Final/ThanhNguyenX/CocktailChoose/CocktailChoose/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CocktailChoose
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 07/19/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+}
---
+++ Examinations and projects/Final/ThanhNguyenX/CocktailChoose/CocktailChoose/Screen/Register/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  CocktailChoose
+//
+//  Created by Thanh Nguyen X. [4] VN.Danang on 07/19/21.
+//
+
+import UIKit
+
+final class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
