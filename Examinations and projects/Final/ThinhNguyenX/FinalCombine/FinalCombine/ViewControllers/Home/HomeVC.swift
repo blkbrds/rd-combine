46d1fc42c51fda46a038d5f84328c7f124a90acb
Thinh Nguyen X <thinh.nguyen2@monstar-lab.com>
2021-07-19 16:15:40 +0700

    Create file

---
+++ Examinations and projects/Final/ThinhNguyenX/FinalCombine/FinalCombine/ViewControllers/Home/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  FinalCombine
+//
+//  Created by Thinh Nguyen X. [2] VN.Danang on 7/19/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
