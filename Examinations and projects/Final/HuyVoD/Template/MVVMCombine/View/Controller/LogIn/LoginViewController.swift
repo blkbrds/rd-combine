0e403498a466d138cedef83dfb14c07d0416d6c6
Huy Vo D.[2] VN.Danang <huy.vo2@monstar-lab.com>
2021-07-29 14:46:11 +0700

    Init login screen

---
+++ Examinations and projects/Final/HuyVoD/Template/MVVMCombine/View/Controller/LogIn/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  MVVMCombine
+//
+//  Created by Huy Vo D. [2] VN.Danang on 7/29/21.
+//  Copyright © 2021 Monstar Lab VietNam Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
